9b76939f5b1887e336c20e9128e1b9544a1e57ca
Program <clement.gauthier132@orange.fr>
2023-08-16 13:57:06 +0200

    Create pridie.py

---
+++ pridie.py
@@ -0,0 +1,43 @@
+# py -m pip install python-nmap
+
+import nmap
+
+title = """
+██████╗░██████╗░██╗██████╗░██╗███████╗
+██╔══██╗██╔══██╗██║██╔══██╗██║██╔════╝
+██████╔╝██████╔╝██║██║░░██║██║█████╗░░
+██╔═══╝░██╔══██╗██║██║░░██║██║██╔══╝░░
+██║░░░░░██║░░██║██║██████╔╝██║███████╗
+╚═╝░░░░░╚═╝░░╚═╝╚═╝╚═════╝░╚═╝╚══════╝
+"""
+
+print(title)
+
+nm = nmap.PortScanner()
+
+def main():
+    waitingEnd = True
+
+    while waitingEnd:
+        num = input("0 - Exit\n1 - Little Network Scanner\n2 - Large Network Scanner\n3 - Silent Little Network Scanner\n4 - Silent Large Network Scanner\n Please enter a number: ")
+
+        if num == 0 or num == "0":
+            print("Good bye")
+            waitingEnd = False
+        elif num == 1 or num == "1":
+            nmap_little()
+        else:
+            print("Wrong number!")
+    return
+
+def get_ip():
+    ip = str(input("\nGive me the IP from the target: "))
+    return ip
+def nmap_little():
+    print("**** Welcome to Little Network Scanner ****")
+    ip = get_ip()
+    nm.scan(ip, '1-443')
+    print(nm.scanInfo())
+    print(nm[ip]['tcp'].keys())
+
+main()
