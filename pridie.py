a65412a553569b9e5ff825f8d1bdd14599cb3cb8
Program <clement.gauthier132@orange.fr>
2023-08-16 14:33:35 +0200

    Update pridie.py

--- pridie.py
+++ pridie.py
@@ -1,6 +1,6 @@
 # py -m pip install python-nmap
 
-import nmap
+import os
 
 title = """
 ██████╗░██████╗░██╗██████╗░██╗███████╗
@@ -19,25 +19,33 @@ def main():
     waitingEnd = True
 
     while waitingEnd:
-        num = input("0 - Exit\n1 - Little Network Scanner\n2 - Large Network Scanner\n3 - Silent Little Network Scanner\n4 - Silent Large Network Scanner\n Please enter a number: ")
+        num = input("0 - Exit\n1 - Network Scanner\n2\n Please enter a number: ")
 
         if num == "0":
             print("Good bye")
             waitingEnd = False
         elif num == "1":
-            nmap_little()
+            NetworkScanner()
         else:
             print("Wrong number!")
     return
 
 def get_ip():
     return input("\nGive me the IP from the target: ")
-def nmap_little():
+
+def NetworkScanner():
     print("**** Welcome to Little Network Scanner ****")
     ip = get_ip()
-    nm.scan(ip, '1-443')
-    open_ports = nm[ip]['tcp'].keys()
-    print(f"Little Network Scan: \n    - Open Ports: {open_ports}")
+    mode = input("\Mode of your scanner: \n1 - Classic and Fast\n2 - Classic and Fast\n3 - Classic and Fast+Silent")
+    command = ""
+    if mode == "1":
+        command = "nmap -T5 --top-ports=10 -sV " + ip
+    elif mode == "2":
+        command = "nmap -T3 --top-ports=10 -sV -n -Pn --disable-arp-ping -D RND:5 " + ip
+    elif mode == "3":
+        command = "nmap -T5 --top-ports=10 -sV -n -Pn --disable-arp-ping -D RND:5 " + ip
+    print(os.system(command))
+    
 
 if __name__ == "__main__":
     main()
