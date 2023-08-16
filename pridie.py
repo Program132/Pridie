eaf9d8db060dd240840cb21219308c1a142e1ad6
Program <clement.gauthier132@orange.fr>
2023-08-16 14:43:13 +0200

    Update pridie.py

--- pridie.py
+++ pridie.py
@@ -17,7 +17,7 @@ def main():
     waitingEnd = True
 
     while waitingEnd:
-        num = input("0 - Exit\n1 - Network Scanner\n2\n Please enter a number: ")
+        num = input("0 - Exit\n1 - Network Scanner\nPlease enter a number: ")
 
         if num == "0":
             print("Good bye")
@@ -34,16 +34,15 @@ def get_ip():
 def NetworkScanner():
     print("**** Welcome to Little Network Scanner ****")
     ip = get_ip()
-    mode = input("\Mode of your scanner: \n1 - Classic and Fast\n2 - Classic and Fast\n3 - Classic and Fast+Silent")
+    mode = input("\nMode of your scanner: \n1 - Classic (Top 10), Silent\n2 - Classic (Top 100), Silent\n3 - All ports, Silent\nEnter a number: ")
     command = ""
     if mode == "1":
-        command = "nmap -T5 --top-ports=10 -sV " + ip
-    elif mode == "2":
         command = "nmap -T3 --top-ports=10 -sV -n -Pn --disable-arp-ping -D RND:5 " + ip
+    elif mode == "2":
+        command = "nmap -T3 -F -sV -n -Pn --disable-arp-ping -D RND:5 " + ip
     elif mode == "3":
-        command = "nmap -T5 --top-ports=10 -sV -n -Pn --disable-arp-ping -D RND:5 " + ip
-    print(os.system(command))
-    
+        command = "nmap -T3 -p- -sV -n -Pn --disable-arp-ping -D RND:5 " + ip
+    print(f"\n Command executed: {command}" + os.system(command) + "\n")
 
 if __name__ == "__main__":
     main()
