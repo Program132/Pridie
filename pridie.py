b827d69ed1d704c7f1f69e1660333ca507a99061
Program <clement.gauthier132@orange.fr>
2023-08-16 14:44:15 +0200

    Update pridie.py

--- pridie.py
+++ pridie.py
@@ -42,7 +42,7 @@ def NetworkScanner():
         command = "nmap -T3 -F -sV -n -Pn --disable-arp-ping -D RND:5 " + ip
     elif mode == "3":
         command = "nmap -T3 -p- -sV -n -Pn --disable-arp-ping -D RND:5 " + ip
-    print(f"\n Command executed: {command}" + os.system(command) + "\n")
+    print(f"\n Command executed: {command} \n {os.system(command)} \n")
 
 if __name__ == "__main__":
     main()
