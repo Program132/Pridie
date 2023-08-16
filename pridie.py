da8793e97f25031ec7ca817d35a810b965bc7168
Program <clement.gauthier132@orange.fr>
2023-08-16 14:50:36 +0200

    Update pridie.py

--- pridie.py
+++ pridie.py
@@ -35,13 +35,17 @@ def NetworkScanner():
     print("**** Welcome to Little Network Scanner ****")
     ip = get_ip()
     mode = input("\nMode of your scanner: \n1 - Classic (Top 10), Silent\n2 - Classic (Top 100), Silent\n3 - All ports, Silent\nEnter a number: ")
+    speed = input("\n Speed of your scan: (1,2,3,4 or 5)")
+    if speed != "1" and speed != "2" and speed != "3" and speed != "4" and speed != "5":
+        print("Error: The speed of the scan must be between 1 and 5!")
+        return
     command = ""
     if mode == "1":
-        command = "nmap -T3 --top-ports=10 -sV -n -Pn --disable-arp-ping -D RND:5 " + ip
+        command = "nmap -T" + speed + " --top-ports=10 -sV -n -Pn --disable-arp-ping -D RND:5 " + ip
     elif mode == "2":
-        command = "nmap -T3 -F -sV -n -Pn --disable-arp-ping -D RND:5 " + ip
+        command = "nmap -T" + speed + " -F -sV -n -Pn --disable-arp-ping -D RND:5 " + ip
     elif mode == "3":
-        command = "nmap -T3 -p- -sV -n -Pn --disable-arp-ping -D RND:5 " + ip
+        command = "nmap -T" + speed + " -p- -sV -n -Pn --disable-arp-ping -D RND:5 " + ip
     print("\n")
     print(f"Command executed: {command}")
     print("\n")
