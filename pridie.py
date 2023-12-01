db941574522237f5e6f2bf049440a6bb0823bba1
Program <67320703+Program132@users.noreply.github.com>
2023-12-01 17:40:38 +0100

    New structure + content

--- pridie.py
+++ pridie.py
@@ -1,6 +1,8 @@
-# py -m pip install python-nmap
+# pip3 install python-nmap
+# pip3 install pymetasploit3
 
 import os
+import platform
 
 title = """
 ██████╗░██████╗░██╗██████╗░██╗███████╗
@@ -13,47 +15,76 @@ title = """
 
 print(title)
 
-def main():
-    waitingEnd = True
-
-    while waitingEnd:
-        num = input("0 - Exit\n1 - Network Scanner\nPlease enter a number: ")
-
-        if num == "0":
-            print("Good bye")
-            waitingEnd = False
-        elif num == "1":
-            NetworkScanner()
+def verificationToolsAndLibs():
+    aMenu = False
+    while not aMenu:
+        accessMenu = str(input("Do you want to install libs to use Pridie? (y/n)"))
+        if accessMenu == "y":
+            print("Installing libs...")
+            os.system("py -m pip install python-nmap")
+            os.system("py -m pip install pymetasploit3")
+            break
+        elif accessMenu == "n":
+            break
         else:
-            print("Wrong number!")
-    return
+            print("Wrong action try again!")
 
-def get_ip():
-    return input("\nGive me the IP from the target: ")
+    aMenu = False
+    while not aMenu:
+        accessMenu = str(input("Do you want to install tools to use Pridie? (y/n)"))
+        if accessMenu == "y":
+            print("Installing tools...")
+            if platform.system() == "Linux":
+                os.system("sudo apt install nmap")
+                os.system("sudo apt install metasploit")
+            elif platform.system() == "Windows":
+                print("NMAP: https://nmap.org/download.html#windows")
+            break
+        elif accessMenu == "n":
+            break
+        else:
+            print("Wrong action try again!")
 
-def NetworkScanner():
-    print("**** Welcome to Little Network Scanner ****")
-    ip = get_ip()
-    mode = input("\nMode of your scanner: \n1 - Classic (Top 10), Silent\n2 - Classic (Top 100), Silent\n3 - All ports, Silent\nEnter a number: ")
-    speed = input("\n Speed of your scan: (1,2,3,4 or 5): ")
-    command = ""
-    
-    if speed != "1" and speed != "2" and speed != "3" and speed != "4" and speed != "5":
-        print("Error: The speed of the scan must be between 1 and 5!")
-        return
-    
-    if mode == "1":
-        command = "nmap -T" + speed + " --top-ports=10 -sV -n -Pn --disable-arp-ping -D RND:5 " + ip
-    elif mode == "2":
-        command = "nmap -T" + speed + " -F -sV -n -Pn --disable-arp-ping -D RND:5 " + ip
-    elif mode == "3":
-        command = "nmap -T" + speed + " -p- -sV -n -Pn --disable-arp-ping -D RND:5 " + ip
-        
-    print("\n")
-    print(f"Command executed: {command}")
-    print("\n")
-    print(os.system(command))
-    print("\n")
+def main():
+    verificationToolsAndLibs()
+
+    over = False
+
+    import nmap
+
+    sc = nmap.PortScanner()
+
+    while not over:
+        inputs = str(input("""
+Select an interaction :
+- 1 : Run Classic Information gathering
+- 2 : Run NMAP Enumeration
+- 3 : Run Metasploit
+- 4 : Quit
+
+Number: """))
+
+        if inputs == "1":
+            print("Running classic information gathering")
+        elif inputs == "2":
+            TargetIP = str(input("Enter the target IP : "))
+            TargetPort = str(input("Enter the port(s) : "))
+            Speed = str(input("Speed of the scan (1-5) : "))
+
+            if Speed != "1" and Speed != "2" and Speed != "3" and Speed != "4" and Speed != "5":
+                print("You did not give a valid speed, the default will be 3.")
+                Speed = 3
+
+            print(f"Running scan on {TargetIP} {TargetPort}")
+            sc.scan(TargetIP, TargetPort, arguments=f"-T{Speed}")
+            result = sc[TargetIP]['tcp']
+            print("Opened ports : ")
+            for i in result.keys():
+                print(f"- {i}, {result[i]['name']}")
+        elif inputs == "3":
+            print("Running Metasploit")
+        elif inputs == "4":
+            over = True
 
 if __name__ == "__main__":
     main()
